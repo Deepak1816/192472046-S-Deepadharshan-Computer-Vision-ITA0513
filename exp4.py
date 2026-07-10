fe9a7aebcc657ee1d8b406345cf8be7e0f1c72e6
Deepadharshan <192472046.simats@saveetha.com>
2026-07-10 11:24:48 +0530

    Add files via upload

---
+++ exp4.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("sample.jpg", 0)
+
+edges = cv2.Canny(img, 100, 200)
+
+kernel = np.ones((5,5), np.uint8)
+
+dilated = cv2.dilate(edges, kernel, iterations=1)
+
+cv2.imshow("Original Image", img)
+cv2.imshow("Dilated Image", dilated)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
